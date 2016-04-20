0682cec660848e9d92d559c0b216eeaa9a0d54cc
Yuancheng Zhang <old.far.hot@gmail.com>
2016-04-19 20:44:01 -0500

    update TCP

--- PA3/PA3_TCP_Student/PA3_Client/PA3_Client/main.cpp
+++ PA3/PA3_TCP_Student/PA3_Client/PA3_Client/main.cpp
@@ -1,9 +1,21 @@
+#define WIN32_LEAN_AND_MEAN
+
 #include <iostream>
 #include <winsock2.h>
-#pragma comment(lib,"ws2_32.lib")
+#include <Windows.h>
+#include <WS2tcpip.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "Trace.h"
 #include "TestData.h"
+
+// Need to link with Ws2_32.lib
+#pragma comment(lib,"ws2_32.lib")
+
+#define DEFAULT_BUFLEN 1024
+#define DEFAULT_PORT 8888
+#define DEFAULT_ACK_MSG "Client: Got the MSG."
 
 int main(void)
 {
@@ -27,8 +39,107 @@ int main(void)
 	// Add your code and hooks here:
 	//--------------------------------------------------------
 
+	// Initialise Winsock
+	WSADATA WsaDat;
+	int iRecvResult;
+	int iSendResult;
+	
+	iSendResult = WSAStartup(MAKEWORD(2, 2), &WsaDat);
+	if (iSendResult != 0)
+	{
+		Trace::out("Winsock error - Winsock initialization failed\r\n");
+		WSACleanup();
+		system("PAUSE");
+		return 1;
+	}
 
+	// Resolve IP address for hostname
+	struct hostent *host;
 
+	if ((host = gethostbyname("localhost")) == NULL)
+	{
+		Trace::out("Failed to resolve hostname.\r\n");
+		WSACleanup();
+		system("PAUSE");
+		return 0;
+	}
+
+	// Create our socket
+	SOCKET Socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (Socket == INVALID_SOCKET)
+	{
+		Trace::out("Winsock error - Socket creation Failed!\r\n");
+		WSACleanup();
+		system("PAUSE");
+		return 0;
+	}
+
+	// Setup our socket address structure
+	SOCKADDR_IN SockAddr;
+	SockAddr.sin_family = AF_INET;
+	SockAddr.sin_addr.s_addr = *((unsigned long*)host->h_addr);
+	SockAddr.sin_port = htons(DEFAULT_PORT);
+
+	// Attempt to connect to server
+	if (connect(Socket, (SOCKADDR*)(&SockAddr), sizeof(SockAddr)) != 0)
+	{
+		Trace::out("Failed to establish connection with server\r\n");
+		WSACleanup();
+		system("PAUSE");
+		return 0;
+	}
+
+	// Send an initial buffer
+	char sendBuffer[DEFAULT_BUFLEN];
+	char recvBuffer[DEFAULT_BUFLEN];
+	memset(sendBuffer, 0, DEFAULT_BUFLEN - 1);
+	memset(recvBuffer, 0, DEFAULT_BUFLEN - 1);
+
+	for (std::list<int>::iterator it = rNodeList.begin(); it != rNodeList.end(); ++it)
+	{
+		iSendResult = send(Socket, (char *)&(*it), sizeof(int), 0);
+		if (iSendResult == SOCKET_ERROR)
+		{
+			Trace::out("Send failed with error: %d\n", WSAGetLastError());
+			closesocket(Socket);
+			WSACleanup();
+			return 1;
+		}
+		Trace::out("Sent: %dB \t---\t Msg: %d\n", iSendResult, *it);
+		iRecvResult = recv(Socket, recvBuffer, DEFAULT_BUFLEN, 0);
+		if (iRecvResult > 0)
+		{
+			Trace::out("Recv: %dB \t---\t Msg: %s\n", iRecvResult, recvBuffer);
+		}
+		else if (iRecvResult == 0)
+		{
+			Trace::out("Connection closed\n");
+		}
+		else
+		{
+			Trace::out("recv failed with error: %d\n", WSAGetLastError());
+		}
+	}
+	
+	// shutdown the connection since no more data will be sent
+	iSendResult = shutdown(Socket, SD_SEND);
+	if (iSendResult == SOCKET_ERROR) {
+		printf("shutdown failed with error: %d\n", WSAGetLastError());
+		closesocket(Socket);
+		WSACleanup();
+		return 1;
+	}
+
+	// Shutdown our socket
+	shutdown(Socket, SD_SEND);
+
+	// Close our socket entirely
+	closesocket(Socket);
+
+	// Cleanup Winsock
+	WSACleanup();
+	system("PAUSE");
+	return 0;
 
 
 	//--------------------------------------------------------
--- PA3/PA3_TCP_Student/PA3_Server/PA3_Server/main.cpp
+++ PA3/PA3_TCP_Student/PA3_Server/PA3_Server/main.cpp
@@ -5,9 +5,11 @@
 #include "Trace.h"
 #include <vector>
 #include <algorithm>
+#include <string>     // std::string, std::stoi
 
-
-
+#define DEFAULT_BUFLEN 1024
+#define DEFAULT_PORT 8888
+#define DEFAULT_ACK_MSG "Server: Got the MSG."
 
 int main()
 {
@@ -15,10 +17,132 @@ int main()
 	// Add your magic here
 	//----------------------------------------------------
 
-	
+	// Initialize Winsock
+	WSADATA WsaDat;
+	int iRecvResult;
+	int iSendResult;
+
+	if (WSAStartup(MAKEWORD(2, 2), &WsaDat) != 0)
+	{
+		Trace::out("WSA Initialzation failed.\r\n");
+		WSACleanup();
+		system("PAUSE");
+		return 0;
+	}
+
+	// Create our socket
+	SOCKET Socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (Socket == INVALID_SOCKET)
+	{
+		Trace::out("Socket creation failed.\r\n");
+		WSACleanup();
+		system("PAUSE");
+		return 0;
+	}
+
+	// Resolve IP address for hostname
 	
 
+	// Setup our socket address structure
+	SOCKADDR_IN serverInf;
+	serverInf.sin_family = AF_INET;
+	serverInf.sin_addr.s_addr = INADDR_ANY;
+	serverInf.sin_port = htons(DEFAULT_PORT);
 
+	// Bind
+	if (bind(Socket, (SOCKADDR*)(&serverInf), sizeof(serverInf)) == SOCKET_ERROR)
+	{
+		Trace::out("Unable to bind socket!\r\n");
+		WSACleanup();
+		system("PAUSE");
+		return 0;
+	}
+
+	// Listen
+	listen(Socket, 1);
+
+	// Server accepts a connection request from a client
+	SOCKET TempSock = SOCKET_ERROR;
+	while (TempSock == SOCKET_ERROR) 
+	{
+		Trace::out("Waiting for incoming connections...\r\n");
+		TempSock = accept(Socket, NULL, NULL);
+	}
+
+	Trace::out("Client connected!\r\n\r\n");
+
+	// No longer need server socket
+	closesocket(Socket);
+
+	// Receive until the peer shuts down the connection
+	char sendBuffer[DEFAULT_BUFLEN];
+	char recvBuffer[DEFAULT_BUFLEN];
+	memset(sendBuffer, 0, DEFAULT_BUFLEN - 1);
+	memset(recvBuffer, 0, DEFAULT_BUFLEN - 1);
+	int recvInt;
+
+	std::vector<int> v;
+	do {
+		iRecvResult = recv(TempSock, (char *)&recvInt, sizeof(int), 0);
+		if (iRecvResult > 0) {
+			v.push_back(recvInt);
+			Trace::out("Recv: %dB \t---\t Msg: %d\n", iRecvResult, recvInt);
+			
+			// send ack message to client
+			iSendResult = send(TempSock, DEFAULT_ACK_MSG, (int)strlen(DEFAULT_ACK_MSG), 0);
+			if (iSendResult == SOCKET_ERROR) {
+				Trace::out("Send failed with error: %d\n", WSAGetLastError());
+				closesocket(TempSock);
+				WSACleanup();
+				system("PAUSE");
+				return 1;
+			}
+			Trace::out("Sent: %dB \t---\t Msg: %s\n", iSendResult, DEFAULT_ACK_MSG);
+		}
+		else if (iRecvResult == 0)
+			Trace::out("Connection closing...\n");
+		else  {
+			Trace::out("recv failed with error: %d\n", WSAGetLastError());
+			closesocket(TempSock);
+			WSACleanup();
+			system("PAUSE");
+			return 1;
+		}
+
+	} while (iRecvResult > 0);
+	Trace::out("Received all numbers\n\n");
+
+	// sort the list
+	std::sort(v.begin(), v.end());
+	Trace::out("Server: Sorted List\n");
+	int j(0);
+	for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it)
+	{
+		Trace::out("    i:%d  %d\n", j++, *it);
+	}
+	
+
+	// shutdown the connection since we're done
+	iRecvResult = shutdown(TempSock, SD_SEND);
+	if (iRecvResult == SOCKET_ERROR) {
+		printf("shutdown failed with error: %d\n", WSAGetLastError());
+		closesocket(TempSock);
+		WSACleanup();
+		system("PAUSE");
+		return 1;
+	}
+
+	// Shutdown our socket
+	shutdown(TempSock, SD_BOTH);
+	shutdown(Socket, SD_BOTH);
+
+	// Close our socket entirely
+	closesocket(TempSock);
+	closesocket(Socket);
+
+	// Cleanup Winsock
+	WSACleanup();
+	
 	//----------------------------------------------------
 	// print and exit
 	//----------------------------------------------------
